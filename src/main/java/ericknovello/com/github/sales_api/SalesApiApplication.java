449b9b9a2d364c95aa86927c8664546a89e38801
Erick Novello <novello.erick@gmail.com>
2023-09-25 21:07:28 -0300

    first commit

---
+++ src/main/java/ericknovello/com/github/sales_api/SalesApiApplication.java
@@ -0,0 +1,13 @@
+package ericknovello.com.github.sales_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ericknovello/com/github/sales_api/SalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ericknovello.com.github.sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
